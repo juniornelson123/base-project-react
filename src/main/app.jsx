d6511d359779b19bf4db417f6b9dafad772c0ebe
juniornelson123 <junior123nelson@gmail.com>
2017-07-17 23:42:19 -0300

    finalizando scaffold

---
+++ src/main/app.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import Routes from './routes'
+import Messages from '../common/msg/messages'
+
+export default props => (
+	<div>
+		<Routes />
+		<Messages /> 
+	</div>
+)
+
